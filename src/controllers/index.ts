bc75c9b1407f244f0477c1864ea391520e26496d
Erick Scur <erickscur@gmail.com>
2022-04-15 21:20:25 -0300

    Brand Controller and Creation

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,3 +1,4 @@
+import { BrandsController } from '../modules/brands/controller/BrandsController';
 import { CategoriesController } from '../modules/categories/controller/CategoriesController';
 
-export const controllers = [new CategoriesController()];
+export const controllers = [new CategoriesController(), new BrandsController()];
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
 import { container } from 'tsyringe';
 import { ICategoriesRepository } from '../../modules/categories/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/categories/repositories/implementations/CategoriesRepository';
+import { BrandsRepository } from '../../modules/brands/repositories/implementations/BrandRepository';
+import { IBrandsRepository } from '../../modules/brands/repositories/IBrandsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
   CategoriesRepository
 );
+
+container.registerSingleton<IBrandsRepository>('BrandsRepository', BrandsRepository);
