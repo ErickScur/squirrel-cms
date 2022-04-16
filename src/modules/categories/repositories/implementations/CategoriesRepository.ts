50f7983b15ca0c0a04200cf2bfbc4bb97789c4cc
Erick Scur <erickscur@gmail.com>
2022-04-16 13:37:33 -0300

    Fix: Error Handling on Repository Class

--- src/modules/categories/repositories/implementations/CategoriesRepository.ts
+++ src/modules/categories/repositories/implementations/CategoriesRepository.ts
@@ -13,42 +13,42 @@ class CategoriesRepository implements ICategoriesRepository {
     try {
       return await CategorySchema.find();
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
   async findById(id: string): Promise<Category> {
     try {
       return await CategorySchema.findById(id);
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
   async findByName(name: string): Promise<Category> {
     try {
       return await CategorySchema.findOne({ name });
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
   async create({ name, description }: ICreateCategoryDTO): Promise<Category> {
     try {
       return await CategorySchema.create({ name, description });
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
   async update({ name, description, id }: IUpdateCategoryDTO): Promise<Category> {
     try {
       return await CategorySchema.findByIdAndUpdate(id, { description, name });
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
   async delete(id: string): Promise<Category> {
     try {
       return await CategorySchema.findByIdAndDelete(id);
     } catch (error) {
-      console.error(error);
+      throw error;
     }
   }
 }
