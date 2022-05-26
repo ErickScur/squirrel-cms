4324eb1ec0f19d961990d59eb0c6a0b1332e847a
Erick Scur <erickscur@gmail.com>
2022-05-26 13:43:03 -0300

    Authentication Middleware

---
+++ src/middlewares/userAuthenticated.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from 'express';
+import { HTTPBadRequest, HTTPForbidden, HTTPNotFound } from '@http/HTTPHandler';
+import { verify } from 'jsonwebtoken';
+import { Environment } from '@environment/index';
+import { UsersRepository } from '@modules/users/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+  admin: boolean;
+}
+export async function authenticationMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+  const jwtSecret = Environment.getConfig('JWT_SECRET');
+  try {
+    if (!authHeader) throw new HTTPBadRequest('Token is missing');
+    const [, token] = authHeader.split(' ');
+    const { sub, admin } = verify(token, jwtSecret) as IPayload;
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(sub);
+    if (!user) throw new HTTPNotFound('User does not exist');
+
+    req.body._user = user;
+    next();
+  } catch (e) {
+    next(e);
+  }
+}
